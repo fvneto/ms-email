44ec4238e21232470a05826e73e4de632ba8bf68
Francisco Vicente A. Neto <vicenteneto.ti@gmail.com>
2021-08-12 08:54:52 -0300

    first commit

---
+++ src/test/java/com/ms/franciscoVicente/msemail/MsEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.franciscoVicente.msemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
